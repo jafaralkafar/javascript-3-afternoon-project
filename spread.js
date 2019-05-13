ef4c129084cc97f5fb8a3327f8ce4808d0ab403f
Grant Fox <granttfox@gmail.com>
2019-05-13 08:44:15 -0600

    finished spread

--- spread.js
+++ spread.js
@@ -18,7 +18,7 @@ let product = {
 }
 
 // code here
-
+var productCopy = {...product}
 
 
 
@@ -29,7 +29,7 @@ let product = {
 let colors = ['red', 'orange', 'yellow', 'green', 'blue', 'indigo', 'violet'];
 
 // code here
-
+var colorsCopy = [...colors]
 
 
 
@@ -45,6 +45,7 @@ function addNums(num1, num2, num3) {
 
 // code here
 
+let result = addNums(...numbers)
 
 ////////// PROBLEM 4 //////////
 
@@ -54,3 +55,4 @@ let arr1 = ['dog', 'chameleon', 'monkey'];
 let arr2 = ['bear', 'tiger', 'liger'];
 
 // code here
+let combinedArrays = [...arr1, ...arr2]
